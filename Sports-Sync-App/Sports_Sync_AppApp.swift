2331c3e8f863cc5ddcd9cbc2ec6f9a41028bf2c9
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2025-04-11 17:13:16 +0530

    Initial Commit

---
+++ Sports-Sync-App/Sports_Sync_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_Sync_AppApp.swift
+//  Sports-Sync-App
+//
+//  Created by ARYAN SINGHAL on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Sports_Sync_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
